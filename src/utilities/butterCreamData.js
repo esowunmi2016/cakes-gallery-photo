bcc3743e1af246176b44156603b8725d4f16b2da
esowunmi2016 <esowunmi2016@fau.edu>
2022-05-31 11:09:06 +0100

    .

---
+++ src/utilities/butterCreamData.js
@@ -0,0 +1,117 @@
+import img1 from '../assets/butterCream/1.jpg';
+import img2 from '../assets/butterCream/2.jpg';
+import img3 from '../assets/butterCream/3.jpg';
+import img4 from '../assets/butterCream/4.jpg';
+import img5 from '../assets/butterCream/5.jpg';
+import img6 from '../assets/butterCream/6.jpg';
+import img7 from '../assets/butterCream/7.jpg';
+import img8 from '../assets/butterCream/8.jpg';
+import img9 from '../assets/butterCream/9.jpg';
+import img10 from '../assets/butterCream/10.jpg';
+import img11 from '../assets/butterCream/11.jpg';
+import img12 from '../assets/butterCream/12.jpg';
+import img13 from '../assets/butterCream/13.jpg';
+import img14 from '../assets/butterCream/14.jpg';
+import img15 from '../assets/butterCream/15.jpg';
+import img16 from '../assets/butterCream/16.jpg';
+
+const butterCreamCakesData= [
+    {
+        id:1,
+        name:'Butter Cream Cakes',
+        img:img1,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:2,
+        name:'Butter Cream Cakes',
+        img:img2,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:3,
+        name:'Butter Cream Cakes',
+        img:img3,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:4,
+        name:'Butter Cream Cakes',
+        img:img4,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:5,
+        name:'Butter Cream Cakes',
+        img:img5,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:6,
+        name:'Butter Cream Cakes',
+        img:img6,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:7,
+        name:'Butter Cream Cakes',
+        img:img7,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:8,
+        name:'Butter Cream Cakes',
+        img:img8,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:9,
+        name:'Butter Cream Cakes',
+        img:img9,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:10,
+        name:'Butter Cream Cakes',
+        img:img10,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:11,
+        name:'Butter Cream Cakes',
+        img:img11,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:12,
+        name:'Butter Cream Cakes',
+        img:img12,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:13,
+        name:'Butter Cream Cakes',
+        img:img13,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:14,
+        name:'Butter Cream Cakes',
+        img:img14,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:15,
+        name:'Butter Cream Cakes',
+        img:img15,
+        link:'/butterCreamCakes'
+    },
+    {
+        id:16,
+        name:'Butter Cream Cakes',
+        img:img16,
+        link:'/butterCreamCakes'
+    },
+]
+
+export default butterCreamCakesData
